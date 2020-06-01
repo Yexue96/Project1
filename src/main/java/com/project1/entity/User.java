ba7431cb3365ab1b084d2816ace59d35d292a199
ziweiwang <ziweiwangap2015@gmail.com>
2020-06-01 18:37:46 -0300

    学习 scope,继承，依赖，p命名空间，工厂方法等ioc特性

--- src/main/java/com/project1/entity/User.java
+++ src/main/java/com/project1/entity/User.java
@@ -1,19 +1,17 @@
 package com.project1.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Data
-public class Student {
+public class User {
     private long id;
     private String name;
     private int age;
     private List<Address> address;
 
-    public Student(){
-        System.out.println("创建了Student");
+    public User(){
+        System.out.println("创建了User");
     }
 }
--- src/main/java/com/project1/test/Test.java
+++ src/main/java/com/project1/test/Test.java
@@ -1,6 +1,7 @@
 package com.project1.test;
 
 import com.project1.entity.Student;
+import com.project1.entity.User;
 import javafx.application.Application;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,9 +15,19 @@ public class Test {
         //System.out.println(student);
 
         //加载配置文件
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring.xml");
-        Student student = (Student) applicationContext.getBean("student2");
-        System.out.println(student);
+       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring.xml");
+//       Student student = (Student) applicationContext.getBean("student");
+//       System.out.println(student);
+//       Student stu = (Student) applicationContext.getBean("stu");
+//       System.out.println(stu);
+//       Student student1 = (Student) applicationContext.getBean("student3");
+//       System.out.println(student == student1);
+//       Student student2 = (Student) applicationContext.getBean("student3");
+//       System.out.println(student == student2);
+
+        User user = (User) applicationContext.getBean("user");
+        System.out.println(user);
+
 
     }
 }
